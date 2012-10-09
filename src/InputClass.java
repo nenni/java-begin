920e14a9c2aea991ddafb441f642996506e466cf
nenni01 <nikolay.nenchev@gmail.com>
2012-10-09 22:28:11 +0300

    new5

--- src/InputClass.java
+++ src/InputClass.java
@@ -7,11 +7,13 @@
  */
 
 import java.util.*;
+import  java.io.*;
 
 public class InputClass {
 
-    public static void main(String[] arg){
-
+    public static void main(String[] arg) throws
+            FileNotFoundException {
+    /*
     Scanner in = new Scanner(System.in);
     System.out.println("Enter your name: ");
     String name = in.next();
@@ -19,6 +21,17 @@ public class InputClass {
     System.out.println("Enter your age: ");
     int age = in.nextInt();
     System.out.printf("%s | %d %n", name, age);
-
+      */
+        File someFile = new File ("someFile .txt");
+        PrintWriter pw = new PrintWriter ( someFile );
+        pw.write ("One \n");
+        pw.write ("Two \n");
+        pw.write ("Three \n");
+        pw.flush ();
+        Scanner infile = new Scanner ( someFile );
+        System.out.println (infile.nextLine ());
+        System.out.println (infile.nextLine ());
+        System.out.println (infile.nextLine ());
+        pw.close();
     }
 }
