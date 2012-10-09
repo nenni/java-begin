604e4bb9f2c363186c0bb151215abe1125da669f
nenni01 <nikolay.nenchev@gmail.com>
2012-10-09 20:26:09 +0300

    First commit

---
+++ src/SimpleClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nenchevn
+ * Date: 10/9/12
+ * Time: 8:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleClass {
+
+    public static void main(String[] arg){
+        System.out.println("Hello Java");
+
+    }
+}
